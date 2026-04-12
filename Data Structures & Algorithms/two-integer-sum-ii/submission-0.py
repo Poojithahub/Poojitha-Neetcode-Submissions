15ffdd40be79adfd78e5bb04912b1759084ef093
Poojithahub <kpooji2000@gmail.com>
2026-04-12 12:36:02 +0530

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers) - 1
+        # here l=0, r= len(numbers)-1= 4-1=3
+
+        while l < r:
+            curSum = numbers[l] + numbers[r]
+            # since given array is in ascending order, if current sum > target, then move to before number of r.
+            # if current sum < target, increase l i.e. move to next number after l
+            if curSum > target:
+                r -= 1
+            elif curSum < target:
+                l += 1
+            else:
+                return [l + 1, r + 1] # in ex1, index 1 & 2 has sum that matches target 3. in result, we returned indexes, not values
+        return []
