8332071778b533cffe424fcf7a7c400544d1ba65
Poojithahub <kpooji2000@gmail.com>
2026-04-12 14:57:58 +0530

    Add: permutation-string - submission-5

---
+++ Data Structures & Algorithms/permutation-string/submission-5.py
@@ -0,0 +1,24 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        # hash map
+        count1 = {}
+        for c in s1:
+            count1[c] = 1 + count1.get(c, 0)
+
+        need = len(count1)
+        for i in range(len(s2)):
+            count2, cur = {}, 0
+            for j in range(i, len(s2)):
+                count2[s2[j]] = 1 + count2.get(s2[j], 0)
+                if count1.get(s2[j], 0) < count2[s2[j]]:
+                    break
+                if count1.get(s2[j], 0) == count2[s2[j]]:
+                    cur += 1
+                if cur == need:
+                    return True
+        return False
+# A permutation of a string simply means it has the exact same characters with the exact same counts, just in a different order.
+
+# abc has a signature of: {a:1, b:1, c:1}.
+
+# cab also has a signature of: {a:1, b:1, c:1}.
